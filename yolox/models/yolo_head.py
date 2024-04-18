d931bae247fe79ffa447d6bd5f62101b1c817bfe
Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>
2024-04-19 00:56:43 +0800

    head: use GIOU loss

--- yolox/models/yolo_head.py
+++ yolox/models/yolo_head.py
@@ -124,7 +124,7 @@ class YOLOXHead(nn.Module):
         self.use_l1 = False
         self.l1_loss = nn.L1Loss(reduction="none")
         self.bcewithlog_loss = nn.BCEWithLogitsLoss(reduction="none")
-        self.iou_loss = IOUloss(reduction="none")
+        self.iou_loss = IOUloss(loss_type="giou", reduction="none")
         self.strides = strides
         self.grids = [torch.zeros(1)] * len(in_channels)
 
